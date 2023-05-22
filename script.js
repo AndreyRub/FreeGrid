2f3d4bc21c8c07bdcb33614800106296eac875bf
Andrey <andrey.rubshtein@gmail.com>
2023-05-22 23:02:11 +0300

    Image carousel

--- script.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', (event) => {
-    document.getElementById("image1").addEventListener("click", function() {
-        new Audio('sounds/sound1.mp3').play();
-    });
-
-    document.getElementById("image2").addEventListener("click", function() {
-        new Audio('sounds/sound2.mp3').play();
-    });
-});
